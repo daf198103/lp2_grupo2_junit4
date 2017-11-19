13995d817825d9e6c23bff49cfe5a6d1645fba46
Robson Sousa <robsoncartes@outlook.com>
2017-11-19 15:51:20 -0200

    first commit - new files added

---
+++ README.md
@@ -0,0 +1,1 @@
+# lp2_grupo2_junit4
---
+++ src/main/java/ana_cristina/MessageUtil.java
@@ -0,0 +1,35 @@
+package ana_cristina;
+
+
+/*
+ *  O conjunto de testes (Test Suite) é usado para agrupar testes de unidade e executá-los
+ *  juntos. No JUnit, as anotações @RunWith e @Suite são usadas para executar os testes do
+ *  conjunto.
+ *
+ *  No exemplo a seguir há duas classes (TestJunit1 e TestJunit2 que são executadas usando o
+ *  Test Suite.
+ *  @param mensagem que será apresentada.
+ */
+
+public class MessageUtil {
+
+    private String message;
+
+    public MessageUtil(String message){
+        this.message = message;
+    }
+
+    public String printMessage(){
+        System.out.println(message);
+
+        return message;
+    }
+
+    public String messagePlusGreeting(){
+
+        message = "Hi!" + message;
+        System.out.println(message);
+
+        return message;
+    }
+}
